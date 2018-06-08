288e2ecee44140e6e5c1a3cb8e058cce292fe529
jerry <jerry.lian@gmail.com>
2018-06-08 11:43:33 -0400

    Simplify codes.

--- hello-client.js
+++ hello-client.js
@@ -1,31 +1,28 @@
 // Hello World client
 // Connects REQ socket to tcp://localhost:5555
 // Sends "Hello" to server.
+var zmq;
+try {
+    console.log(require.resolve("zmq"));
+    zmq = require('zmq');
+} catch(e) {
+    console.error("zmq is not found, change to zeromq");
+    zmq = require('zeromq');
+}
 
-var zmq = require('zmq');
+console.log("Connecting to hello-world server…");
+var requester = zmq.socket('req');// socket to talk to server
 
-// socket to talk to server
-console.log("Connecting to hello world server…");
-var requester = zmq.socket('req');
-
-var x = 0;
 requester.on("message", function(reply) {
-  console.log("Received reply", x, ": [", reply.toString(), ']');
-  x += 1;
-  if (x === 10) {
+    console.log("Received reply", 0, ": [", reply.toString(), ']');
     requester.close();
     process.exit(0);
-  }
 });
 
 requester.connect("tcp://localhost:5555");
 //requester.connect("tcp://10.0.0.2:5555");
 
-for (var i = 0; i < 10; i++) {
-  console.log("Sending request", i, '…');
-  requester.send("Hello");
-}
+console.log("Sending request", 0, '…');
+requester.send("Hello");
 
-process.on('SIGINT', function() {
-  requester.close();
-});
+process.on('SIGINT', function() {  requester.close(); });
--- hello-server.js
+++ hello-server.js
@@ -1,29 +1,26 @@
 // Hello World server
 // Binds REP socket to tcp://*:5555
 // Expects "Hello" from client, replies with "world"
+var zmq;
+try {
+    console.log(require.resolve("zmq"));
+    zmq = require('zmq');
+} catch(e) {
+    console.error("zmq is not found, change to zeromq");
+    zmq = require('zeromq');
+}
 
-var zmq = require('zmq');
-
-// socket to talk to clients
-var responder = zmq.socket('rep');
+var responder = zmq.socket('rep');// socket to talk to clients
 
 responder.on('message', function(request) {
-  console.log("Received request: [", request.toString(), "]");
-
-  // do some 'work'
-  setTimeout(function() {
-
+    console.log("Received request: [", request.toString(), "]");
     // send reply back to client.
     responder.send("World");
-  }, 1000);
 });
 
 responder.bind('tcp://*:5555', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Listening on 5555…");
-  }
+   if (err)     console.log(err);
+   else         console.log("Listening on 5555…");
 });
 
 process.on('SIGINT', function() {
